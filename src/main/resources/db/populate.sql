ec14364ebfce26f7e57af675615340a4ad75ba06
dzhin1993 <zhinkoilya1993@gmail.com>
2020-06-11 21:26:25 +0300

    initialize db

---
+++ src/main/resources/db/populate.sql
@@ -0,0 +1,5 @@
+INSERT INTO users VALUES (1, 'user', 'user@mail.ru', 'user', '2010-10-20 10:10');
+
+INSERT INTO todo VALUES (1, 'first', '2010-10-20 13:00', FALSE, 1);
+INSERT INTO todo VALUES (2, 'second', '2010-10-20 18:00', FALSE, 1);
+INSERT INTO todo VALUES (3, 'third', '2010-10-20 21:00', FALSE, 1);
