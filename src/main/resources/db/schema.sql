7863b894350591639465be7988b2c2066583f7bf
dzhin1993 <zhinkoilya1993@gmail.com>
2020-06-13 15:57:58 +0300

    remove users and security

--- src/main/resources/db/schema.sql
+++ src/main/resources/db/schema.sql
@@ -1,21 +1,21 @@
 DROP TABLE todo IF EXISTS;
-DROP TABLE users IF EXISTS;
+/*DROP TABLE users IF EXISTS;*/
 
-CREATE TABLE users (
+/*CREATE TABLE users (
   id         INTEGER IDENTITY PRIMARY KEY,
   name       VARCHAR(255) NOT NULL,
   email      VARCHAR(255) NOT NULL,
   password   VARCHAR(255) NOT NULL,
   registered TIMESTAMP    NOT NULL
-);
+);*/
 
 CREATE TABLE todo (
   id        INTEGER IDENTITY PRIMARY KEY,
   name      VARCHAR(255) NOT NULL,
   date_time TIMESTAMP    NOT NULL,
-  completed BOOLEAN      NOT NULL,
+  completed BOOLEAN      NOT NULL/*,
   user_id   INTEGER      NOT NULL,
-  FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
+  FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE*/
 );
 
 CREATE UNIQUE INDEX todo_date_time ON todo (date_time);
