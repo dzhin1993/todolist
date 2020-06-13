81e94edb142ab2d518bf1dfe4559ae41e4292156
dzhin1993 <zhinkoilya1993@gmail.com>
2020-06-13 13:16:36 +0300

    add controller

---
+++ src/main/java/com/zhinkoilya1993/todolist/model/Controller/TodoController.java
@@ -0,0 +1,40 @@
+package com.zhinkoilya1993.todolist.model.Controller;
+
+import com.zhinkoilya1993.todolist.repository.TodoRepository;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@Controller
+@RequestMapping(value = TodoController.URL)
+public class TodoController {
+
+    static final String URL = "/";
+
+    private final TodoRepository repository;
+
+    public TodoController(TodoRepository repository) {
+        this.repository = repository;
+    }
+
+    @GetMapping
+    public String getAll(Model model,
+                         @PageableDefault(sort = {"dateTime"}, direction = Sort.Direction.ASC) Pageable pageable) {
+        model.addAttribute("todos", repository.findAll(pageable));
+        return "index";
+    }
+
+    @GetMapping("/by-completed")
+    public String getAllByCompleted(Model model,
+                         @PageableDefault(sort = {"dateTime"}, direction = Sort.Direction.ASC) Pageable pageable,
+                                    @RequestParam(required= false, defaultValue = "false") Boolean completed) {
+        model.addAttribute("todos", repository.findAllByCompleted(pageable, completed));
+        return "index";
+    }
+}
--- src/main/resources/db/populate.sql
+++ src/main/resources/db/populate.sql
@@ -1,5 +1,17 @@
 INSERT INTO users VALUES (1, 'user', 'user@mail.ru', 'user', '2010-10-20 10:10');
 
-INSERT INTO todo VALUES (1, 'first', '2010-10-20 13:00', FALSE, 1);
-INSERT INTO todo VALUES (2, 'second', '2010-10-20 18:00', FALSE, 1);
-INSERT INTO todo VALUES (3, 'third', '2010-10-20 21:00', FALSE, 1);
+INSERT INTO todo VALUES (1, 'day_1_task_1', '2010-10-1 13:00', FALSE, 1);
+INSERT INTO todo VALUES (2, 'day_1_task_2', '2010-10-1 18:00', FALSE, 1);
+INSERT INTO todo VALUES (3, 'day_1_task_3', '2010-10-1 21:00', FALSE, 1);
+
+INSERT INTO todo VALUES (4, 'day_2_task_1', '2010-10-2 13:00', FALSE, 1);
+INSERT INTO todo VALUES (5, 'day_2_task_2', '2010-10-2 18:00', FALSE, 1);
+INSERT INTO todo VALUES (6, 'day_2_task_3', '2010-10-2 21:00', FALSE, 1);
+
+INSERT INTO todo VALUES (7, 'day_3_task_1', '2010-10-3 13:00', FALSE, 1);
+INSERT INTO todo VALUES (8, 'day_3_task_2', '2010-10-3 18:00', FALSE, 1);
+INSERT INTO todo VALUES (9, 'day_3_task_3', '2010-10-3 21:00', FALSE, 1);
+
+INSERT INTO todo VALUES (10, 'day_4_task_1', '2010-10-4 13:00', FALSE, 1);
+INSERT INTO todo VALUES (11, 'day_4_task_2', '2010-10-4 18:00', FALSE, 1);
+INSERT INTO todo VALUES (12, 'day_4_task_3', '2010-10-4 21:00', FALSE, 1);
