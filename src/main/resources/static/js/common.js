33440db7d10a98154b3f384abefeaba16fd105b3
dzhin1993 <zhinkoilya1993@gmail.com>
2020-06-21 11:03:23 +0300

    add ajax

---
+++ src/main/resources/static/js/common.js
@@ -0,0 +1,9 @@
+function complete(id, completed) {
+    $.ajax({
+        type: 'POST',
+        url: '/todoList/complete',
+        data: { id: id, completed : completed}
+    }).done(function () {
+        location.reload();
+    });
+}
