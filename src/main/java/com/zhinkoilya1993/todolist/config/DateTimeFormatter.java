1d24ccff25e4d7035355b9d8cc349f577e882248
dzhin1993 <zhinkoilya1993@gmail.com>
2020-07-20 20:23:49 +0300

    fixes

--- src/main/java/com/zhinkoilya1993/todolist/config/DateTimeFormatter.java
+++ src/main/java/com/zhinkoilya1993/todolist/config/DateTimeFormatter.java
@@ -3,7 +3,6 @@ package com.zhinkoilya1993.todolist.config;
 import org.springframework.format.Formatter;
 import org.springframework.util.StringUtils;
 
-import java.text.ParseException;
 import java.time.LocalDateTime;
 import java.util.Locale;
 
@@ -11,7 +10,7 @@ import java.util.Locale;
 public class DateTimeFormatter implements Formatter<LocalDateTime> {
 
     @Override
-    public LocalDateTime parse(String str, Locale locale) throws ParseException {
+    public LocalDateTime parse(String str, Locale locale) {
         return StringUtils.isEmpty(str) ? null : LocalDateTime.parse(str);
     }
 
