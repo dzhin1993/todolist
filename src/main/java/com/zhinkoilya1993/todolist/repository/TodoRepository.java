298738441daecaf1ef60c978261fadb7b9e4867a
dzhin1993 <zhinkoilya1993@gmail.com>
2020-08-06 11:54:58 +0300

    fix repository

--- src/main/java/com/zhinkoilya1993/todolist/repository/TodoRepository.java
+++ src/main/java/com/zhinkoilya1993/todolist/repository/TodoRepository.java
@@ -1,9 +1,11 @@
 package com.zhinkoilya1993.todolist.repository;
 
 import com.zhinkoilya1993.todolist.model.Todo;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -16,6 +18,8 @@ public interface TodoRepository extends CrudRepository<Todo, Integer> {
     @Query("SELECT t FROM Todo t WHERE t.user.id=?1")
     List<Todo> getAll(int userId);
 
+    @Transactional
+    @Modifying
     @Query("DELETE FROM Todo t WHERE t.id=?1 AND t.user.id=?2")
     void delete(int id, int userId);
 }
