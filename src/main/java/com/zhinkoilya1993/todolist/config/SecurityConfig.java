e506bbba59afb364bbecba411718374c347a66d2
dzhin1993 <zhinkoilya1993@gmail.com>
2020-08-24 14:10:11 +0300

    add logout

--- src/main/java/com/zhinkoilya1993/todolist/config/SecurityConfig.java
+++ src/main/java/com/zhinkoilya1993/todolist/config/SecurityConfig.java
@@ -37,7 +37,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                     .formLogin()
                     .loginPage("/login")
                     .defaultSuccessUrl("/todoList", true)
-                    .permitAll();
+                    .permitAll()
+                .and()
+                    .logout()
+                    .logoutSuccessUrl("/login");
     }
 
     @Bean
